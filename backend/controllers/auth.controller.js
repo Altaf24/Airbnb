dc73a4a87edc3b7ce94f6e29a6f069061e659aad
ALTAF TAMBOLI <144025502+Altaf24@users.noreply.github.com>
2025-07-18 22:36:33 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -15,7 +15,7 @@
 //         res.cookie("token",token,{
 //             httpOnly:true,
 //             secure:true,
-//             sameSite: "",
+//             sameSite: "none",
 //             maxAge: 7 * 24 * 60 * 60 * 1000
 
 
@@ -42,7 +42,7 @@
 //         res.cookie("token",token,{
 //             httpOnly:true,
 //             secure:true,
-//             sameSite: "",
+//             sameSite: "none",
 //             maxAge: 7 * 24 * 60 * 60 * 1000
 
 
